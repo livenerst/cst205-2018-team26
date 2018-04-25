fa8d46bc4b876ce02f6ae3626bc32c053c8751ac
Joffrey Chambon <chambon.joffrey@neuf.fr>
2018-04-25 15:47:50 -0700

    joff_filter 1.2

--- joff_filter.py
+++ joff_filter.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
-image = cv2.imread('rouge.jpg')
+image = cv2.imread('photo_de_profil.jpg',cv2.IMREAD_GRAYSCALE)
 def filter1 (image) :
     height, width = image.shape[:2]
     for y in range (height) :
         for x in range (width) :
             pixel = image[y,x]
-            for z in range (len(pixel)) :
-                pixel [z] = (0.4*pixel[0]+4*pixel[1]+1.2*pixel[2])/4
+            pixel[0] = (0.4*pixel[0]+4*pixel[1]+1.2*pixel[2])/4
+            pixel[1] = (0.4*pixel[0]+4*pixel[1]+1.2*pixel[2])/4
+            pixel[2] = (0.4*pixel[0]+4*pixel[1]+1.2*pixel[2])/4
             image[y,x] = pixel
     return image
 def grey_filter_progressive (image) :
@@ -32,13 +33,32 @@ def grey_filter (image) :
     return image
 def revert_filter (image) :
     height, width = image.shape[:2]
-    for y in range (width) :
-        for x in range (height) :
+    for y in range (int(width)) :
+        for x in range (int(height)) :
             temp = image [x,y]
             image [x,y] = image [-x,-y]
-            image[-x,-y] = temp
+            image[x-height,y-width] = temp
+    return image
+def filter_column_average (image) :
+    height, width = image.shape[:2]
+    for y in range (width) :
+        averageR = 0
+        averageG = 0
+        averageB = 0
+        for x in range (height) :
+            pixel = image[x,y]
+            averageR += pixel[0]
+            averageG += pixel[1]
+            averageB += pixel[2]
+        averageR /= height
+        averageG /= height
+        averageB /= height
+        for x in range (height) :
+            pixel = image[x,y]
+            pixel = [averageR,averageG,averageB]
+            image[x,y] = pixel
     return image
 temp = image
-cv2.imshow('image',filter1(temp))
+cv2.imshow('image',laplace_filter(temp,laplace))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
